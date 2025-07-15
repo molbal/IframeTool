66780dc7bc72e0c4e460c30875cfcd123cc21c1d
molbal <molbal@outlook.com>
2025-07-15 21:29:58 +0200

    Add initial implementation of IframeTool for Laravel Nova with Vue components, CSS, and API routes. Include necessary files like .gitignore, package.json, and webpack.mix.js for setup

---
+++ resources/js/pages/Tool.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Head title="Grafana charts" />
+
+      <iframe id="iframe"
+        src="/graphs/index?embed=true"
+        width="100%"
+        height="1000"
+        frameborder="0"></iframe>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    //
+  },
+}
+function resizeIFrameToFitContent( iFrame ) {
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    var iFrame = document.getElementById( 'iframe' );
+    resizeIFrameToFitContent( iFrame );
+} );
+</script>
+
+<style>
+/* Scoped Styles */
+</style>
---
+++ src/ToolServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Molbal\IframeTool;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Http\Middleware\Authenticate;
+use Laravel\Nova\Nova;
+use Molbal\IframeTool\Http\Middleware\Authorize;
+
+class ToolServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $this->routes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            //
+        });
+    }
+
+    /**
+     * Register the tool's routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        if ($this->app->routesAreCached()) {
+            return;
+        }
+
+        Nova::router(['nova', Authenticate::class, Authorize::class], 'iframe_tool')
+            ->group(__DIR__.'/../routes/inertia.php');
+
+        Route::middleware(['nova', Authorize::class])
+            ->prefix('nova-vendor/iframe_tool')
+            ->group(__DIR__.'/../routes/api.php');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
